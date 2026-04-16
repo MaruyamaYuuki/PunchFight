d49ea4e1481b2fc7e1c55eab9fa31be5ae292e4c
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2026-04-16 12:49:33 +0900

    クラスコメント・関数コメントの追加

--- DirectXGame/App/Objects/PLayer/PlayerTextureController.h
+++ DirectXGame/App/Objects/PLayer/PlayerTextureController.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <cstdint>
 
+/// <summary>
+/// プレイヤーの論理的な行動状態を定義する列挙型
+/// </summary>
 enum class PlayerState {
 	Idle,
 	Move,
@@ -11,10 +14,27 @@ enum class PlayerState {
 	Victory
 };
 
+/// <summary>
+/// プレイヤーのテクスチャリソース管理と、状態に応じたハンドルの切り替えを担当するクラス。
+/// 左右の向き、アニメーションフレーム、攻撃の左右判定、タイマーによる表情変化などを
+/// 一括して引き受け、描画に必要なテクスチャハンドルを決定します。
+/// </summary>
 class PlayerTextureController {
 public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize();
 
+	/// <summary>
+	/// 現在のプレイヤーの状況を入力し、描画すべき最適なテクスチャハンドルを取得する。
+	/// </summary>
+	/// <param name="isRight">プレイヤーが右を向いているか</param>
+	/// <param name="state">現在の行動ステート</param>
+	/// <param name="walkFrame">移動アニメーションの現在のフレーム番号</param>
+	/// <param name="attackFromRight">（攻撃中のみ）右パンチか左パンチか</param>
+	/// <param name="knockDownTimer">（死亡中のみ）経過時間によるダウン状態の遷移用タイマー</param>
+	/// <returns>描画に使用するテクスチャハンドル</returns>
 	uint32_t GetTexture(bool isRight, PlayerState state, int walkFrame, bool attackFromRight, float knockDownTimer);
 
 private:
